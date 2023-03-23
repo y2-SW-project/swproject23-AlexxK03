4e5f2fe28e1256247847e79b53599d4494e62cc5
Alex Killian <N00212207@iadt.ie>
2023-03-23 18:33:52 +0000

    routing working for view all but trips not being defined correctly

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TripController;
+use App\Models\Trip;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,7 +13,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('trips.index');
 
 Route::Resource('/trips',TripController::class)->middleware(['auth']);
 
