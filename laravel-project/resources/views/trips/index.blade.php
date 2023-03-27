8092f226d1852fdca2918d5d74f1e3776a0c0ad8
Alex Killian <N00212207@iadt.ie>
2023-03-27 12:03:05 +0100

    aestethic changes to index

--- laravel-project/resources/views/trips/index.blade.php
+++ laravel-project/resources/views/trips/index.blade.php
@@ -4,21 +4,23 @@
     <div>
         <h2 class="d-flex justify-content-center">Your Trips</h2>
     </div>
-    <div class="row ps-3">
-        <div class="col-md-4">
-            @forelse($trips as $trip)
-                <div class="card">
-                    <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png" class="card-img-top"
-                        alt="...">
-                    <div class="card-body">
-                        <h3 class="card-title"><strong>Destination: </strong>{{ $trip->destination }}<h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md d-flex">
+                @forelse($trips as $trip)
+                    <div class="card mx-2">
+                        <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-social.png" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title"><strong>Destination: </strong>{{ $trip->destination }}<h3>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
                     </div>
-                </div>
-            @empty
-                <p>You have no trips logged</p>
-            @endforelse
+                @empty
+                    <p>You have no trips logged</p>
+                @endforelse
+            </div>
         </div>
     </div>
 @endsection
